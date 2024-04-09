a8de798d8b2adabb6aa60857993f31128626787b
Alpaca4610 <461065836@qq.com>
2024-04-09 13:32:56 +0800

    update gemini_webapi

--- nonebot_plugin_bard/gemini_webapi/__init__.py
+++
@@ -1,3 +0,0 @@
-from .client import GeminiClient, ChatSession  # noqa: F401
-from .exceptions import *  # noqa: F401, F403
-from .types import *  # noqa: F401, F403
--- nonebot_plugin_bard/gemini_webapi/types/__init__.py
+++
@@ -1,3 +0,0 @@
-from .image import Image, WebImage, GeneratedImage  # noqa: F401
-from .candidate import Candidate  # noqa: F401
-from .modeloutput import ModelOutput  # noqa: F401
